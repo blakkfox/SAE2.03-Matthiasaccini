3e181ce42ccf850aa92afa3d9e9a1aff4f18b046
Accini Matthias <accini1@mmi-dev.unilim.fr>
2026-04-22 13:20:13 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "accini1");
+define("DBLOGIN", "accini1");
+define("DBPWD", "accini1");
 
 
 function getAllMovies(){
