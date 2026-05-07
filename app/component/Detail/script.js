882aa0a7ff7a9cde7ea582fcbbb6cd33d323845d
Accini Matthias <accini1@mmi-dev.unilim.fr>
2026-05-07 13:33:31 +0200

    rendu final

--- app/component/Detail/script.js
+++ app/component/Detail/script.js
@@ -3,17 +3,26 @@ let template = await templateFile.text();
 
 let Detail = {};
 
-Detail.format = function (data, css = "") {
+Detail.format = function (data, categoryName, css = "") {
   let html = template;
   html = html.replaceAll("{{cssClass}}", css);
   html = html.replaceAll("{{name}}", data.name);
   html = html.replaceAll("{{image}}", "../server/images/" + data.image);
   html = html.replaceAll("{{director}}", data.director);
   html = html.replaceAll("{{year}}", data.year);
-  html = html.replaceAll("{{id_category}}", data.id_category);
-  html = html.replaceAll("{{min_age}}",data.min_age);
+  html = html.replaceAll("{{categoryName}}", categoryName);
+  html = html.replaceAll("{{min_age}}", data.min_age);
   html = html.replaceAll("{{description}}", data.description);
-  html = html.replaceAll("{{trailer}}", data.trailer);
+
+  let urlVideo = data.trailer || "";
+  if (urlVideo.includes("watch?v=")) {
+      urlVideo = urlVideo.replace("watch?v=", "embed/");
+  } 
+  else if (urlVideo.includes("youtu.be/")) {
+      urlVideo = urlVideo.replace("youtu.be/", "youtube.com/embed/");
+  }
+  html = html.replaceAll("{{trailer}}", urlVideo);
+  
   return html;
 };
 
